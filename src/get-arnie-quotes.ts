ffd9bca4ffe7d76b863a439c8160d644e8044289
Aleksei Pushkin <jewelstd@gmail.com>
2024-05-06 15:11:52 +1000

    1. Puzzle implementation.

--- src/get-arnie-quotes.ts
+++ src/get-arnie-quotes.ts
@@ -1,10 +1,29 @@
 import { httpGet } from './mock-http-interface';
 
-// TODO define this type properly
-type TResult = any;
-
-export const getArnieQuotes = async (urls : string[]) : Promise<TResult[]> => {
-  // TODO: Implement this function.
-  
-  return [];
+type TResultSuccess = {
+  'Arnie Quote': string;
 };
+
+type TResultFailure = {
+  FAILURE: string
+};
+
+type TResult = TResultSuccess | TResultFailure;
+
+const toTResult = (p:  PromiseSettledResult<{status: number; body: string;}>): TResult => {
+  if(p.status === 'fulfilled') {
+    const { body: bodyJson, status } = p.value;
+    const body: { message: string } = JSON.parse(bodyJson);
+    if(status === 200) {
+      return { 'Arnie Quote': body.message };
+    }
+    return { FAILURE: body.message };
+  }
+  // I am not sure what we expect in this case since httpGet never rejects, so will be a bit defensive. :)
+  return { FAILURE: `Unexpected promise rejection: ${p.reason}` };
+};
+
+// Since most of the logic is just the conversion betwen settled results and TResults, I found it reasonable to keep this function as a one-liner.
+// It seems readable enough for me and I would extend it if anyone would complain in a PR comment.
+export const getArnieQuotes = async (urls : string[]) : Promise<TResult[]> =>
+  (await Promise.allSettled(urls.map(httpGet))).map(toTResult);
